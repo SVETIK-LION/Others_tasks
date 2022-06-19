13e7501d5bf9330a7692d27fe0baa7ea0efd07c9
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-06-19 13:24:52 +0500

    change phrase_generation.py

--- phrase_generation.py
+++ phrase_generation.py
@@ -28,6 +28,6 @@ def generate_phrase(name_1: str, name_2: str, damage: int, weapon: str, type_att
 
 print(generate_phrase('Единорожка', 'Муся-Пуся', 14, 'Рог', 'ATACK'))
 print(generate_phrase('Ололошка', 'Кентавриха', 6, 'СвинцовоеКопыто', 'FAIL'))
-print(generate_phrase('ЧеткийГепард', 'БогСолнца', 10, 'ЛучиДобра', 'BLOCK'))
+print(generate_phrase('ЧеткийГепард', 'БогСолнца', 0, 'ЛучиДобра', 'BLOCK'))
 print(generate_phrase('Кошка', 'Собака', 8, 'Когти', 'no_name'))
 
